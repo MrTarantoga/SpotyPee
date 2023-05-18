6f8f6ff3a338c2928bc2821377a723710cdc7300
Goetz <goetz-dev@web.de>
2023-05-18 22:39:02 +0200

    Update: New Yocto version
    - Currently no new meta-raspberrypi has been released, so fixed master
    branch is used.

--- init_spotypee.sh
+++ init_spotypee.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 RESULT_FOLDER="spotypee_build"
-YOCTO_VERSION="langdale"
+YOCTO_VERSION="mickledore"
 GIT_YOCTO="git://git.yoctoproject.org/poky.git"
 GIT_OPENEMBEDDED="git://git.openembedded.org/meta-openembedded"
 GIT_RASPBERRYPI="git://git.yoctoproject.org/meta-raspberrypi"
 GIT_SECURITY="git://git.yoctoproject.org/meta-security.git"
 
-GIT_REPOS=("$GIT_OPENEMBEDDED" "$GIT_RASPBERRYPI" "$GIT_SECURITY")
-
+#GIT_REPOS=("$GIT_OPENEMBEDDED" "$GIT_RASPBERRYPI" "$GIT_SECURITY")
+GIT_REPOS=("$GIT_OPENEMBEDDED" "$GIT_SECURITY")
 
 git clone --branch $YOCTO_VERSION $GIT_YOCTO $RESULT_FOLDER
 cd $RESULT_FOLDER
+
+echo $GIT_RASPBERRYPI
+git clone --branch master $GIT_RASPBERRYPI
+
+cd meta-raspberrypi
+# Checkout fixed master state
+git checkout 928bb234bb788b2723658df57e06e663f6426538
+cd ..
+
 for git_repo in "${GIT_REPOS[@]}";
 do
-	echo $git_repo
-	git clone --branch $YOCTO_VERSION $git_repo
+  echo $git_repo
+  git clone --branch $YOCTO_VERSION $git_repo
 done
 
 rm -rf meta-poky
